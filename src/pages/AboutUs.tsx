b87944ede88c728a98b74c2d3d080be089416b5e
marlin <marlin.arai@gmail.com>
2025-02-22 19:09:38 +0900

    aboutusページ基礎づくり

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,12 +1,25 @@
 import './AboutUs.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import img from '../images/Aboutus/aboutus_img.png';
 const AboutUs = () => {
   return (
   <>
     <Header />
     <div className="about-us">
-      <h1>私たちについて</h1>
+      <h1>About us</h1>
+      <div>
+        <div className='aboutusimg'>
+          <img src={img} alt="img" />
+          <img src={img} alt="img" />  
+        </div>
+        <h2>Create</h2>
+        <p>開発
+        <br/>設計
+        <br/>デザイン
+        <br/>実装</p>
+      </div>
+      
     </div>
     <Footer />
   </>
