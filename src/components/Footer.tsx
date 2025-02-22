84945e81df7e383d68fc648d67c1f32916ce0585
kakaz-one <bousomusume2410836@gmail.com>
2025-02-22 16:22:37 +0900

    フッターとヘッダーの変更。また、topページの中身も変更。div-1とdiv-2を作成。

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Headerlogo from '../images/Header/Headerlogo.svg';
+import InstagramLogo from '../images/Footer/instagram_logo.svg';
+import XLogo from '../images/Footer/X_logo.svg';
 import './Footer.css';
 
 const Footer: React.FC = () => {
@@ -10,6 +12,19 @@ const Footer: React.FC = () => {
       <div className="footer-container">
         <div className="logo">
           <img src={Headerlogo} alt="Logo" />
+        </div>
+        <div className="social-media">
+          <div className="instagram">
+          <a href="https://www.instagram.com/gdgs_chuo?igsh=MWdxNzFhdmRvZDNveg==" target="_blank" rel="noopener noreferrer">
+            <img src={InstagramLogo} alt="Instagram" />
+          </a>
+          </div>
+          <div className='follow-us'>Follow us</div>
+          <div className="X">
+            <a href="https://x.com/gdgs_chuo?s=21&t=xSTSR4dUHIM8W0zT6brq9w" target="_blank" rel="noopener noreferrer">
+              <img src={XLogo} alt="X" />
+            </a>
+          </div>
         </div>
         <nav className="footer-menu">
           <ul>
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,13 +2,38 @@ import './TopPage.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import gdgslogo from '../images/TopPage/gdgslogo.svg';
+import div2img from '../images/TopPage/div2img.svg';
 const TopPage = () => {
   return (
   <>
     <Header />
     <div className="top-page">
+      <div className="div-1">
+        <div className='div-1-img'>
+          <img src={gdgslogo} alt="TopPageImage" /> 
+        </div>
+        <div className='div-1-text'>
+          <h1>We are GDGs Chuo</h1>
+        </div>
+
+      </div>
+      <div className='div-2'>
+        <div className='div-2-img'>
+          <img src={div2img} alt="TopPageImage" /> 
+        </div>
+        <div className='div-2-text'>
+          <h2>GDSC_Chuoは</h2>
+          <h2>Google Developer Student Clubsの中央大学支部として</h2>
+          <h2>2022年に設立されました</h2>
+          <h2>テクノロジーに興味のある人が集まり</h2>
+          <h2>日々活動をしています</h2>
+        </div>
+      </div>
+      
+        
+      
      
-        <img src={gdgslogo} alt="TopPageImage" />
+        
       
       <h1>トップページです</h1>
     </div>
