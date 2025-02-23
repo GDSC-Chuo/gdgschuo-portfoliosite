7e1a793f19018346b113f7949ea8e5a51629e8c0
kakaz-one <bousomusume2410836@gmail.com>
2025-02-24 00:16:00 +0900

    topページのinfoセクションの中身を作成。レスポンシブ対応も

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -3,6 +3,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import gdgslogo from '../images/TopPage/gdgslogo.svg';
 import div2img from '../images/TopPage/div2img.svg';
+import instagramlogo from '../images/Footer/instagram_logo.svg';
+import Xlogo from '../images/Footer/X_logo.svg';
 const TopPage = () => {
   return (
   <>
@@ -31,6 +33,35 @@ const TopPage = () => {
       </div>
       <div className='info-section'>
         <h2>Information</h2>
+        <table className="info-table">
+          <tbody>
+            <tr>
+              <th>公式名称</th>
+              <td>GDGs Chuo</td>
+            </tr>
+            <tr>
+              <th>創設</th>
+              <td>2022</td>
+            </tr>
+            <tr>
+              <th>主な活動拠点</th>
+              <td>中央大学市谷田町キャンパス</td>
+            </tr>
+            <tr>
+              <th>SNS</th>
+              <td>
+                <span className="sns-account">
+                  <img src={Xlogo} alt="XLogo" />
+                  gdgs_chuo
+                </span>
+                <span className="sns-account">
+                  <img src={instagramlogo} alt="InstagramLogo" />
+                  @GDGs_chuo
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       
         
